8c7edad15a73683c9fe4ce7b0326b6556df1bf7b
asuforce <owata.sn@gmail.com>
2019-10-18 19:32:01 +0900

    Output csv

--- channel_getter/channel_getter.py
+++ channel_getter/channel_getter.py
@@ -20,10 +20,16 @@ def main():
     with open("channel_list.json", "w") as f:
         json.dump(channels, f)
 
-    with open("channel_list.csv", "w") as f:
+    with open("channel_list.csv", "w") as c:
+        w = csv.writer(c)
 
-        w = csv.writer(f)
-        w.writerow(channels)
+        count = 0
+        for channel in channels:
+            if count == 0:
+                w.writerow(["name"])
+                count += 1
+
+            w.writerow([channel["name"]])
 
 
 if __name__ == "__main__":
